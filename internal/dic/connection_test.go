e4555f4b491614a379bf8b60a834cd4cd1b639de
ikawaha <ikawaha@users.noreply.github.com>
2015-04-19 09:56:50 +0900

    Add tests

---
+++ internal/dic/connection_test.go
@@ -0,0 +1,39 @@
+//  Copyright (c) 2015 ikawaha.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package dic
+
+import (
+	"testing"
+)
+
+func TestConnectionTableAt(t *testing.T) {
+	var ct ConnectionTable
+	const (
+		row = 4
+		col = 5
+	)
+	ct.Row = row
+	ct.Col = col
+	ct.Vec = make([]int16, ct.Row*ct.Col)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			ct.Vec[i*col+j] = int16(i*col + j)
+		}
+	}
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			expected := int16(i*col + j)
+			if r := ct.At(i, j); r != expected {
+				t.Errorf("got %v, expected %v\n", r, expected)
+			}
+		}
+	}
+
+}
