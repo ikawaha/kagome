a81776e75ebaeadf7d818817f907d0a8fccad707
ikawaha <ikawaha@users.noreply.github.com>
2015-10-29 14:26:30 +0900

    Fix tests

--- splitter/splitter_test.go
+++ splitter/splitter_test.go
@@ -12,25 +12,45 @@ func TestDefaultSplit(t *testing.T) {
 		input  string
 		expect []string
 	}{
-		{input: "あああ", expect: []string{"あああ"}},
-		{input: "。。．", expect: []string{"。。．"}},
-		{input: "あいう「かきく」えお。abcd", expect: []string{"あいう「かきく」えお。", "abcd"}},
-		{input: "あああ。いいい", expect: []string{"あああ。", "いいい"}},
-		{input: "あああ．いいい．", expect: []string{"あああ．", "いいい．"}},
-		{input: "   あああ．    　いいい。", expect: []string{"あああ．", "いいい。"}},
-		{input: "   あああ．\nいいい。", expect: []string{"あああ．", "いいい。"}},
-		{input: "   あああ．\nいいい。", expect: []string{"あああ．", "いいい。"}},
-		{input: "  あああ\n\nいいい。", expect: []string{"あああ", "いいい。"}},
-		{input: "  あ あ  あ。\n\n。いいい。", expect: []string{"あああ。", "。", "いいい。"}},
+		{
+			input:  "",
+			expect: []string{},
+		},
+		{
+			input:  "あああ",
+			expect: []string{"あああ"},
+		},
+		{
+			input:  "。。．",
+			expect: []string{"。。．"},
+		},
+		{
+			input:  "こんにちはは「さようなら」。\U0001f363が好き",
+			expect: []string{"こんにちはは「さようなら」。", "\U0001f363が好き"},
+		},
+		{
+			input:  "こんにちは。さようなら。おはよう．おやすみ．",
+			expect: []string{"こんにちは。", "さようなら。", "おはよう．", "おやすみ．"},
+		},
+		{
+			input:  "　 こ んに  ち　は。　 ．」　 さ\vよ\rう\tな\nら\n",
+			expect: []string{"こんにちは。．」", "さようなら"},
+		},
+		{
+			input:  "\n\nこんにちは\n\nさようなら。\n\n",
+			expect: []string{"こんにちは", "さようなら。"},
+		},
+		{
+			input:  "  こんにちは。。」さようなら。』ごきげんよう",
+			expect: []string{"こんにちは。。」", "さようなら。』", "ごきげんよう"},
+		},
 	}
 
 	for _, d := range testdata {
 		scanner := bufio.NewScanner(strings.NewReader(d.input))
-		scanner.Split(spliter.ScanSentences)
+		scanner.Split(splitter.ScanSentences)
 		r := make([]string, 0, len(d.expect))
 		for scanner.Scan() {
-			//sen := scanner.Text()
-			//r = append(r, sen)
 			r = append(r, scanner.Text())
 		}
 		if !reflect.DeepEqual(r, d.expect) {
