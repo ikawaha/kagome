d07a05431e3a89e1505fa073abb10cb419d37fac
ikawaha <ikawaha@users.noreply.github.com>
2015-10-29 16:15:02 +0900

    Add license comments

--- splitter/splitter.go
+++ splitter/splitter.go
@@ -1,3 +1,17 @@
+// Copyright 2015 ikawaha
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 	You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package splitter
 
 import (
@@ -25,7 +39,7 @@ var (
 	}
 )
 
-// ScanSentences is a split function for a Scanner that returns each sentece of text.
+// ScanSentences is a split function for a bufio.Scanner that returns each sentece of text.
 func ScanSentences(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	return defaultSpliter.ScanSentences(data, atEOF)
 }
