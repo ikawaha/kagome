78fadfec0098c487c3d4e4d0fc337dc0564b686e
ikawaha <ikawaha@users.noreply.github.com>
2015-12-24 12:51:28 +0900

    Use "circo" instead of "dot" to draw the lattice graph

--- cmd/kagome/server/cmd.go
+++ cmd/kagome/server/cmd.go
@@ -199,7 +199,10 @@ func (h *TokenizeDemoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	const cmdTimeout = 15 * time.Second
+	const (
+		graphvizCmd = "circo" // "dot"
+		cmdTimeout  = 15 * time.Second
+	)
 	var (
 		records []record
 		tokens  []tokenizer.Token
@@ -214,7 +217,7 @@ func (h *TokenizeDemoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 	case "extended":
 		m = tokenizer.Extended
 	}
-	if _, e := exec.LookPath("dot"); e != nil {
+	if _, e := exec.LookPath(graphvizCmd); e != nil {
 		cmdErr = "Error: graphviz is not in your furure"
 		log.Print("graphviz is not in your future\n")
 	} else {
